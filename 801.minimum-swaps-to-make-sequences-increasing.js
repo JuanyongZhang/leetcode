b3be762215b444e6273115807cc6fd7298ebd097
Juanyong Zhang <j.zhang@servicepower.com>
2020-03-15 22:30:55 -0400

    add more

--- 801.minimum-swaps-to-make-sequences-increasing.js
+++ 801.minimum-swaps-to-make-sequences-increasing.js
@@ -12,22 +12,40 @@
  */
 var minSwap = function (A, B) {
     const len = A.length;
-    if (len === 1)
-        return 1;
-    if (len === 2) {
-        A[0], B[0] = B[0], A[0];
-        console.dir({ A, B });
+    const keep = Array(len - 1).fill(Infinity);
+    const swap = Array(len - 1).fill(Infinity);
+    keep.unshift(0);
+    swap.unshift(1);
+    for (let i = 1; i < len; i++) {
+        if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+            keep[i] = keep[i - 1]; //no swap for i and i-1
+            swap[i] = swap[i - 1] + 1; //swap for both i and i-1
+        }
+        if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+            //swap
+            swap[i] = Math.min(swap[i], keep[i - 1] + 1);//swap i
+            keep[i] = Math.min(keep[i], swap[i - 1]); //swap i-1
+        }
     }
 
+
+    return Math.min(swap[len - 1], keep[len - 1]);
 };
 // @lc code=end
+console.table([
+    [1, 3, 5, 4],
+    [1, 2, 3, 7],
+])
 
 console.log(
     minSwap(
-        [5, 4],
-        [3, 7]
-        // [1, 3, 5, 4],
-        // [1, 2, 3, 7]
+        [1, 3, 5, 4],
+        [1, 2, 3, 7]
     )
 )
-
+/*
+Accepted
+102/102 cases passed (60 ms)
+Your runtime beats 65.49 % of javascript submissions
+Your memory usage beats 100 % of javascript submissions (36.9 MB)
+*/
---
+++ Passed/740.delete-and-earn.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=740 lang=javascript
+ *
+ * [740] Delete and Earn
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn = function (nums) {
+    const buff = Array(10000).fill(0);
+    let len = 0;
+    for (const num of nums) {
+        buff[num] += num;
+        len = Math.max(len, num);
+    }
+    // console.table(buff);
+    const memo = new Map();
+    const dp = n => {
+        if (memo.has(n)) return memo.get(n);
+        if (n < 0) return 0;
+        memo.set(n, Math.max(dp(n - 2) + buff[n], dp(n - 1)));
+        return memo.get(n);
+    }
+    return dp(len);
+};
+// @lc code=end
+
+console.log(deleteAndEarn(
+    [3, 4, 2]
+));
+console.log(deleteAndEarn(
+    [2, 2, 3, 3, 3, 9]
+));
+
+/*
+Accepted
+46/46 cases passed (60 ms)
+Your runtime beats 67.5 % of javascript submissions
+Your memory usage beats 100 % of javascript submissions (37 MB)
+*/
